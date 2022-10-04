50e3225473ef4da0c26f65b0a80593de8181a139
Edilma Riano <edilmaria@hotmail.com>
2022-10-04 08:51:27 -0500

    split string

--- october_2022/Program.cs
+++ october_2022/Program.cs
@@ -8,35 +8,174 @@ namespace october_2022
 {
     class Program
     {
+        //interface IRandomizable
+        //{
+        //    double GetRandomNum(double upperBoundNumber);
+        //}
+
+
+        //public class RandomNumber: IRandomizable
+        //{
+
+        //    public RandomNumber() { }
+            
+                
+        //    public double GetRandomNum( double upperBoundNumber)
+        //    {
+        //        Random rnd = new Random();
+        //        double myrandom = rnd.NextDouble() * upperBoundNumber;
+        //        return myrandom;
+        //    }
+        //}
+
+
+
+
+
+
         static void Main(string[] args)
         {
 
 
-            // Reverse a string//
-           //string reverseString = "world";
-           //Console.WriteLine(Solution(reverseString));
-           // Console.WriteLine ("// ----- //");
-           // Console.WriteLine(SolutionOptimize(reverseString));
 
-            string sentence = "This is an example";
+            // Exercise of 
+            //bool sali = false;
+            //RandomNumber number = new RandomNumber();
+            //string str;
+            //do
+            //{
+            //    Console.WriteLine("Enter a number for the upper bound");
+            //    str = Console.ReadLine();
+            //    try
+            //    {
+            //        double upperBound = Double.Parse(str);
+            //        Console.WriteLine($"Random Number between 0 and {str} : {number.GetRandomNum(upperBound)}");
+            //    }
+            //    catch (Exception e) { };
+
+
+            //} while (str!="exit");
+
+
+
+
+
+            // RandomNumber number = new RandomNumber();
+            //number.GetRandomNum(num);
+
+            //Console.WriteLine(number.GetRandomNum(num));
+
+
+            // Console.WriteLine((num));
+
+
+
+
+
+
+
+            // Reverse a string//
+            //string reverseString = "world";
+            //Console.WriteLine(Solution(reverseString));
+            // Console.WriteLine ("// ----- //");
+            // Console.WriteLine(SolutionOptimize(reverseString));
+
+            //string sentence = "This is an example";
             //string[] newArray = sentence.Split(' ');
             //string otro="";
             //Array.Reverse(newArray);
             //for (int i = 0; i < newArray.Length; i++)
             //{
             //    otro = newArray[i]+ " " + otro;
-               
+
             //}
 
 
             //Console.WriteLine("{0}", otro);
 
-            Console.WriteLine(ReverseWords(sentence));
+            // Console.WriteLine(ReverseWords(sentence));
+
+            string mysample = "abcdefg";
+            string[] answer = Solution(mysample);
+
+          /*
+            string[] answer = Solution(mysample);
+            char[] mywords = mysample.ToArray();
+
+            int count = mywords.Length;
+            string[] newArray = new string[(count / 2)];
+            int helper = 0;
+
+
+            for (int index = 0; index < mywords.Length - 1; index += 2)
+            {
+                string twochar = new string(mywords, index, 2);
+                newArray[helper] = twochar;
+                helper++;
+                
+                //Console.WriteLine(twochar);
+
+
+            }
+          */
+
+            foreach (var item in answer)
+            {
+                Console.WriteLine(item);
+            }
 
 
 
+            /*
+                //Console.WriteLine(answer);
+                foreach (var item in answer)
+            {
+                Console.WriteLine(item);
+            }
+
+            */
 
             Console.ReadLine();
+        }
+
+
+        public static string[] Solution(string str)
+        {
+            char[] mywords = str.ToArray();
+            int count = str.Length;
+            
+            List<string> thewords = new List<string>();
+                
+            int helper = 0; 
+            
+
+            //check if the string has even number of characters
+            if (count % 2 != 0)
+            {
+                int newLenght = (count / 2) + 1;
+                int j = 0;
+                
+                for (int index = 0; index < mywords.Length - 1; index += 2)
+                {
+                    string twochar = new string(mywords, index, 2);
+                    thewords[j] = twochar;
+                    j++;
+                }
+                thewords[j+1] = mywords[count + 1] + "_";
+                return thewords.ToArray();
+
+
+            }
+            string[] newArray = new string[(count / 2)];
+
+            // if the numbers of characters is odd the last item ends with _
+            for (int index = 0; index < mywords.Length - 1; index += 2)
+            {
+                string twochar = new string(mywords, index, 2);
+                newArray[helper] = twochar;
+                helper++;
+            }
+            return newArray;
         }
 
         public static string ReverseWords(string str)
@@ -59,7 +198,7 @@ namespace october_2022
 
 
         // Function to reverse a string ( my take)
-        public static string Solution(string str)
+        public static string Solutionxxx(string str)
         {
             char[] newArray = str.ToArray();
             Array.Reverse(newArray);
@@ -111,6 +250,10 @@ namespace october_2022
 
  
         }
+        //Complete the solution so that it splits the string into pairs of two characters.
+        //If the string contains an odd number of characters then it should replace the missing
+        //second character of the final pair with an underscore ('_').
+      
 
 
 
---
+++ splitstrings/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splitstrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testEven = "abcdefg";
+            //string testOdd = "abcdefg";
+            string[] resultEven = Solution(testEven);
+            // string[] resultOdd = Solution(testOdd);
+
+            foreach (var item in resultEven)
+            {
+                Console.WriteLine(item);
+            }
+            //for (int i = 0; i < testEven.Length; i++)
+            //{
+            //    Console.WriteLine("This is the result odd {0}", resultOdd[i]);
+            //}
+            Console.ReadLine();
+            
+        }
+        public static string[] Solution(string str)
+        {
+            int lenghtStr = str.Length;
+            if (lenghtStr%2 ==0)
+            {
+                return StringEven(str);
+            }
+            else
+            {
+                string newString = str+ "_";
+                Console.WriteLine(newString);
+                return StringEven(newString);
+            }
+
+
+        }
+
+        public static string[] StringEven(string str)
+        {
+            char[] arrayChars = str.ToCharArray();
+            List<string> resultList = new List<string>();
+            for (int i = 0; i < str.Length-1; i+=2)
+            {
+                string coupleChar = new string(arrayChars, i, 2);
+                resultList.Add(coupleChar);
+            }
+            return resultList.ToArray();
+        }
+
+    }
+}
