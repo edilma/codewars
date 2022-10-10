9f87dce89291baf52e6fbaca896cc13a3a04bed9
Edilma Riano <edilmaria@hotmail.com>
2022-10-10 11:16:35 -0500

    MoveZeros using quauaes

---
+++ Move_Zeros/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Zeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myOriginal = { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+            int[] result = new int[myOriginal.Length];
+            result = MoveZeroes(myOriginal);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            Console.ReadLine();
+        }
+        public static int[] MoveZeroes(int[] arr)
+        {
+            List<int> myList = new List<int>();
+                        
+            Queue<int> mynumbers = new Queue<int>();
+            int countOfZeros = 0;
+            int arrayLenght = arr.Length;
+            // Cree un queaeu with the non zero members
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]!=0)
+                {
+                    mynumbers.Enqueue(arr[i]);
+                    
+                }
+                else
+                {
+                    countOfZeros += 1;
+                }
+            }
+            //Add the nonZero quaue members to the list
+            foreach (int item in mynumbers)
+            {
+                myList.Add(item);
+            }
+            //Complete the list with the zeros
+            int countQueau = mynumbers.Count;
+            for (int i = countQueau-1; i < arrayLenght-1; i++)
+            {
+                myList.Add(0);
+            }
+            
+            return myList.ToArray();
+        }
+    }
+}
---
+++ Move_Zeros/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Move_Zeros")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Move_Zeros")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf899b6b-b5fc-4ae4-bb14-e0586e88dd49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
