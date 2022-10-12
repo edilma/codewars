d882ccc59d4ea682b95e73f0633371b4724c6970
Edilma Riano <edilmaria@hotmail.com>
2022-10-12 11:32:38 -0500

    Adding big numbers - only long

---
+++ october_2022/AddingBigNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add("123456789", "987654322"));
+
+            Console.ReadLine();
+
+        }
+        
+         public static string Add(string a, string b)
+        {
+            // The biggest number will be a long int.  
+            // sum the digits of the digits of the string
+            long numa;
+            long numb;
+            Int64.TryParse(a, out numa);
+            Int64.TryParse(b, out numb);
+
+            long total = numa + numb;
+
+            // convert the numbers to string and return it
+            string result = total.ToString();
+
+            return result;
+ 
+        }
+
+    }
+}
