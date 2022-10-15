c09fc9d10911270ddc6c379208b5ba08264c25a4
Edilma Riano <edilmaria@hotmail.com>
2022-10-14 20:27:43 -0500

    Perfect Power

---
+++ october_2022/PerfectPower/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            Console.WriteLine(IsPerfectPower(num));
+            Console.ReadLine();
+
+        }
+        public static (int, int)? IsPerfectPower(int n)
+        {
+            // math.power(base, power)
+            int kPower=2;
+            int mBase=2;
+            while (mBase<n)
+            {
+                do
+                {
+                    if (Math.Pow(mBase, kPower) == n)
+                    {
+                        return (mBase, kPower);
+                    }
+                   mBase++;
+                } while (kPower<n);
+
+
+                kPower++;
+
+            }
+           
+            return null;
+        }
+
+        
+        
+    }
+}
