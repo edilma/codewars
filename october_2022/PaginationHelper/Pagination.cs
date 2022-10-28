08aefaebe0a18b6943eefcc4babb0edf648e8956
Edilma Riano <edilmaria@hotmail.com>
2022-10-28 17:49:46 -0500

    working with css

--- october_2022/PaginationHelper/Pagination.cs
+++ october_2022/PaginationHelper/Pagination.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace PaginationHelper
 {
-    public class PagnationHelper<T>
+    public class PagnationHelper11<T>
     {
         IList<T> _collection;
         int _itemsPerPage;
-        public PagnationHelper(IList<T> collection, int itemsPerPage)
-        {
-            _collection = collection;
-            _itemsPerPage = itemsPerPage;
+        //public PagnationHelper(IList<T> collection, int itemsPerPage)
+        //{
+        //    _collection = collection;
+        //    _itemsPerPage = itemsPerPage;
 
 
-        }
+        //}
 
         /// <summary>
         /// The number of items within the collection
@@ -34,10 +34,11 @@ namespace PaginationHelper
         /// </summary>
         public int PageCount
         {
+
             get
             {
-                int pages = ItemCount/ _itemsPerPage;
-                return (int)Math.Ceiling(pages);  
+                int pages = (int)Math.Ceiling( Convert.ToDouble(ItemCount)/ Convert.ToDouble (_itemsPerPage));
+                return pages;  
                
             }
         }
@@ -45,12 +46,15 @@ namespace PaginationHelper
         /// <summary>
         /// Returns the number of items in the page at the given page index 
         /// </summary>
-        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
-        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+
+    
+            /// 
+            /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+            /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
         public int PageItemCount(int pageIndex)
         {
 
-            if (pageIndex> PageCount || pageIndex<0)
+            if (pageIndex> PageCount - 1 || pageIndex<0)
             {
                 return -1;
             }
@@ -58,11 +62,11 @@ namespace PaginationHelper
             {
                 if (pageIndex != PageCount-1)
                 {
-                    return PageCount;
+                    return _itemsPerPage;
                 }
                 else
                 {
-                    return ItemCount % PageCount ;
+                    return ItemCount % _itemsPerPage ;
                 }
             }
 
