264cf24a30e80f8701dd8e8448b070fed0622eb9
Edilma Riano <edilmaria@hotmail.com>
2022-10-30 15:35:35 -0500

    format number array into phone number

--- october_2022/Multiply/Program.cs
+++ october_2022/Multiply/Program.cs
@@ -15,22 +15,39 @@ namespace Multiply
 
             //Console.WriteLine( CustomMath.multiply(a, b));
 
-            List<Object> newList = new List<object> { "hello", 1, 8, "my house", 9, true, 8 };
-            IEnumerable<int> result = ListFilterer.GetIntegersFromList(newList);
+            //List<Object> newList = new List<object> { "hello", 1, 8, "my house", 9, true, 8 };
+            // IEnumerable<int> result = ListFilterer.GetIntegersFromList(newList);
             //foreach (var  item in newList)
             //{
             //    Console.WriteLine(item.GetType().ToString());
             //}
-            foreach (int item in result)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (int item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
+            int[] anArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+           
+           Console.WriteLine(CreatePhoneNumber(anArray));
+            //Console.WriteLine(phoneNumber);
 
 
             Console.ReadLine();
         }
-        
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            //My solution 
+
+            return  $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+
+
+            //return long.Parse(string.Concat(numbers)).ToString("(000) 000-0000");
+
+
+
+        }
+
 
 
     }
@@ -46,20 +63,23 @@ namespace Multiply
     {
         public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
         {
-            int a = 5;
-            List<object> myResult = new List<object>();
-            List<int> newList = new List<int>();
+           return listOfItems.OfType<int>();
+
             
-            //IEnumerable<int> results = new IEnumerable<int>();
-            foreach (var item in listOfItems)
-            {
-                if (item.GetType() == a.GetType())
-                {
-                    newList.Add((int)item);
-                }
+
+            // My solution
+            //int a = 5;
+            //List<int> newList = new List<int>();
+            
+            //foreach (var item in listOfItems)
+            //{
+            //    if (item.GetType() == a.GetType())
+            //    {
+            //        newList.Add((int)item);
+            //    }
                 
-            }
-            return newList;
+            //}
+            //return newList;
             
         }
     }
