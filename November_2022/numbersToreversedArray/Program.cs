2672913d30e576db47342e0e747fe746e70b11e8
Edilma Riano <edilmaria@hotmail.com>
2022-11-02 10:14:45 -0500

    reverse number finished

--- November_2022/numbersToreversedArray/Program.cs
+++ November_2022/numbersToreversedArray/Program.cs
@@ -10,7 +10,7 @@ namespace numbersToreversedArray
     {
         static void Main(string[] args)
         {
-            long number= 0;
+            long number= 56000;
             long[] result = Digitize(number);
             foreach (int item in result)
             {
@@ -22,44 +22,26 @@ namespace numbersToreversedArray
         }
         public static long[] Digitize(long n)
         {
-            bool falta = true;
+            
             List<long> result = new List<long>();
             if (n == 0)
             {
                 result.Add(0);
                 return result.ToArray();
             }
-            else {
-                if (n >= 1)
+            else 
+            {
+                while (n>=1)
                 {
-                    long next = n % 10;
-                    result.Add(next);
+                    result.Add(n % 10);
                     n = n / 10;
+
                 }
+                return result.ToArray();
             };
-
-
-
-
-
+          
+        }
             
-            else { 
-                while (falta)
-                {
-                    if (n%10>=1)
-                    {
-                        long next = n % 10;
-                        result.Add(next);
-                        n = n/10;
-                    }
-                    else
-                    {
-                        falta = false;
-                    }
-               
-                }
-            }
-            return result.ToArray();
         }
     }
-}
+
