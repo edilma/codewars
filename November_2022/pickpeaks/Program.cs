bff7a4e0ffa0f06ce953f0e3651da717303c542b
Edilma Riano <edilmaria@hotmail.com>
2022-11-10 19:44:29 -0500

    days work

--- November_2022/pickpeaks/Program.cs
+++ November_2022/pickpeaks/Program.cs
@@ -12,24 +12,64 @@ namespace pickpeaks
         {
             int[] test = { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3 };
             int[] test2 = { 1, 2, 2, 2, 1 };
-            Dictionary<string, List<int>> result = GetPeaks(test2);
+            // Dictionary<string, List<int>> result = GetPeaks(test2);
 
+            Console.WriteLine(test2[0]);
 
-
-            foreach (var item in result)
-            {
-                Console.WriteLine(item.Key);
-                foreach (var value in item.Value)
-                {
-                    Console.WriteLine(value);
-                }
-            }
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    foreach (var value in item.Value)
+            //    {
+            //        Console.WriteLine(value);
+            //    }
+            //}
 
 
             Console.ReadLine();
 
         }
+
+
+
+
+
+
         public static Dictionary<string, List<int>> GetPeaks(int[] arr)
+        {
+           // List<int> ogArray = new List<int>(arr);
+            Dictionary<string, List<int>> solution = new Dictionary<string, List<int>>();
+            List<int> peaks = new List<int>();
+            List<int> posInt = new List<int>();
+            solution.Add("pos:", posInt);
+            solution.Add("peaks:", peaks);
+            int prevPos = 0;
+            int currentPos = 0;
+            for (int nextPos = 1; nextPos < arr.Length-1; nextPos++)
+            {
+                if (arr[nextPos] >arr[prevPos])
+                {
+                    currentPos = nextPos;
+                    
+                }
+            }
+
+
+
+
+
+
+
+
+
+            return null;
+
+
+        }
+        
+
+
+            public static Dictionary<string, List<int>> GetPeaks2(int[] arr)
         {
             List<int> ogArray = new List<int>(arr);
             Dictionary<string, List<int>> solution = new Dictionary<string, List<int>>();
@@ -37,7 +77,6 @@ namespace pickpeaks
             List<int> posInt = new List<int>();
 
 
-     
             if (ogArray.Count<=2)
             {
                 solution.Add("pos:", posInt);
@@ -49,7 +88,7 @@ namespace pickpeaks
                 int current = 1;
                 int before = 0;
                 int after = 2;
-                for (int i = 1; i < ogArray.Count - 1; i++)
+                for (int i = current; i < ogArray.Count - 1; i++)
                 {
                     
                     if (isPeak(ogArray[current], ogArray[before], ogArray[after]))
