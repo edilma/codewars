456bf90b78a677c93747ccd48350ca0860bc3e77
Edilma Riano <edilmaria@hotmail.com>
2022-11-20 00:12:42 -0500

    replace alphabeth with position

--- November_2022/squareEveryDigit/Program.cs
+++ November_2022/squareEveryDigit/Program.cs
@@ -10,26 +10,64 @@ namespace squareEveryDigit
     {
         static void Main(string[] args)
         {
-            int num = 91188;
+            //int num = 811181;
             //Console.WriteLine(num * num);
-            Console.WriteLine(SquareDigits(num));
+            //Console.WriteLine(SquareDigits(num));
+            string word = "The sunset sets at twelve o' clock.";
+            string word2 = "h";
+            Console.WriteLine(AlphabetPosition(word));
+
+            //int ind = word.IndexOf('s');
+            //Console.WriteLine(ind);
+
+            //string str = "GeeksForGeeks";
+            
+            //foreach (char item in str)
+            //{
+            //    int index1 = str.IndexOf(item);
+            //    if (str.Contains(item))
+            //    {
+            //        Console.WriteLine("The Index Value of character 'k' is " + index1);
+            //    }
+            //}
+            
+
             Console.ReadLine();
 
         }
-
-        public static int SquareDigits(int num)
+        public static string AlphabetPosition(string text)
         {
-            //string digs = num.ToString();
-            //char[] arr = digs.ToArray();
-            bool isFinish = false;
+            string letters = text.ToUpper();
+
+            char[] alph = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        
+            string ans=string.Empty;
+            foreach (char letter in letters)
+            {
+               
+                if (alph.Contains(letter))
+                {
+                    int index = Array.IndexOf(alph,letter)+1;
+                    ans = ans+" " +  index.ToString();
+                }
+
+            }
+
+
+            return ans;
+        }
+
+        public static int SquareDigits(int n)
+        {
+
             List<int> digits = new List<int>();
             string ans="";
 
-            while (num%10>=1)
+            while (n%10>=1)
             {
-                int sq = (num % 10) ;
-                digits.Add(sq *sq);
-                num = num / 10;
+               int sq = (n % 10) ;
+               digits.Add(sq *sq);
+                n = n / 10;
 
             }
             for (int i = digits.Count-1; i >=0; i--)
@@ -37,18 +75,14 @@ namespace squareEveryDigit
                 ans = ans + digits[i].ToString();
                
             }
-            //  Console.WriteLine(ans);
+            
             int answer; 
             int.TryParse(ans, out answer);
             return answer;
 
-            //foreach (int item in digits)
-            //{
-            //    Console.WriteLine("the item is {0}", item);
-            //}
-
-
-            return -1;
+           
         }
+
+       
     }
 }
